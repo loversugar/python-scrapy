a029f94d0ac3a9396ec33abdd14c770714012ef5
totti <253262275@qq.com>
2019-09-16 21:08:19 +0800

    step3

--- tutorial/spiders/ quotes_spider.py
+++ tutorial/spiders/ quotes_spider.py
@@ -21,9 +21,34 @@ class QutoesSpider(scrapy.Spider):
     #     for url in urls:
     #         yield scrapy.Request(url=url, callback=self.parse)
 
+    # def parse(self, response):
+    #     page = response.url.split("/")[-2]
+    #     filename = 'qutoes-%s.html' % page
+    #     with open(filename, 'wb') as f:
+    #         f.write(response.body)
+    #     self.log('Saved file %s' % filename)
+
+    # def parse(self, response):
+    #     for quote in response.css('div.quote'):
+    #         yield {
+    #             'text': quote.css('span.text::text').get(),
+    #             'author': quote.css('small.author::text').get(),
+    #             'tags': quote.css('div.tags a.tag::text').getall(),
+    #         }
+
+    #     next_page = response.css('li.next a::attr(href)').get()
+    #     if (next_page) is not None:
+    #         next_page = response.urljoin(next_page)
+    #         yield scrapy.Request(next_page, callback=self.parse)
+
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'qutoes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if (next_page) is not None:
+            yield response.follow(next_page, callback=self.parse)
